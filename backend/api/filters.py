6ddc558a97d61752d55df468f41f2e4aa22fb0f2
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-10-29 20:28:57 +0500

    First review, cycle 3 - add recipe_limit - for recipes - add limit param for @action - subscriptions

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,12 +3,15 @@ from django_filters import rest_framework as filters
 from recipes.models import Recipe
 
 
-# class TitleFilter(filters.FilterSet):
-#     category = filters.CharFilter(field_name='category__slug')
-#     genre = filters.CharFilter(field_name='genre__slug')
-#     name = filters.CharFilter(field_name="name", lookup_expr='icontains')
-#     year = filters.NumberFilter(field_name='year')
-#
-#     class Meta:
-#         model = Title
-#         fields = ('category', 'genre', 'name', 'year')
+class RecipeFilter(filters.FilterSet):
+    tags = filters.CharFilter(field_name='tags__slug')
+    # category = filters.CharFilter(field_name='category__slug')
+    # genre = filters.CharFilter(field_name='genre__slug')
+    # name = filters.CharFilter(field_name="name", lookup_expr='icontains')
+    # year = filters.NumberFilter(field_name='year')
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'tags',
+        )
--- backend/api/views/recipes.py
+++ backend/api/views/recipes.py
@@ -10,6 +10,7 @@ from rest_framework import filters, permissions, serializers, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from api.filters import RecipeFilter
 from api.paginations import LimitResultsSetPagination
 from api.serializers.recipes import (FavoriteSerializer, IngredientSerializer,
                                      RecipeSerializerWrite, RecipeSerializer,
@@ -40,6 +41,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('tags',)
+    filterset_class = RecipeFilter
     pagination_class = LimitResultsSetPagination
 
     def get_serializer_class(self):

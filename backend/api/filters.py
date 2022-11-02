5f0349a4c14512e7f54260e4d44c831d49fefe8c
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-11-03 04:54:44 +0500

    - workflow - add push to server

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,6 +4,7 @@ from recipes.models import Recipe
 
 class RecipeFilter(filters.FilterSet):
     tags = filters.CharFilter(field_name='tags__slug')
+    # name = filters.CharFilter(method='get_name')
     author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
@@ -15,7 +16,7 @@ class RecipeFilter(filters.FilterSet):
         model = Recipe
         fields = (
             'author',
-            'name'
+            # 'name'
             'tags',
             'is_favorited',
             'is_in_shopping_cart'

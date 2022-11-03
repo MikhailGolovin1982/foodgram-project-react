e0b8d254eb45ebb45b6c2114c0d9b83327fcc134
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-11-03 09:40:10 +0500

    - workflow - add push to server

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filters.FilterSet):
@@ -12,7 +12,11 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.MultipleChoiceFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
     author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(

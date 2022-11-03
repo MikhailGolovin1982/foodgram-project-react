b1aa6b6a08a942d21138e3845a1938d72280594a
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-11-03 08:16:56 +0500

    - workflow - add push to server

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-
 from api.views.recipes import IngredientViewSet, RecipeViewSet, TagViewSet
 from api.views.users import UserViewSet
 

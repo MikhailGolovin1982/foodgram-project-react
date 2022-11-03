441f5b0c5c33b1af2b6d05fb3749cce526bcba28
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-11-03 08:53:59 +0500

    - workflow - add push to server

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,7 +12,7 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.CharFilter(field_name='tags__slug')
+    tags = filters.MultipleChoiceFilter(field_name='tags__slug')
     author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(

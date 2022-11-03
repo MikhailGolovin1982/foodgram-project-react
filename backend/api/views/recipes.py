1fcadbf998dc435717d8fe6c5763edd5f9c06757
Mikhail Golovin <76044731+MikhailGolovin1982@users.noreply.github.com>
2022-11-03 10:07:32 +0500

    - ingredient filter - repair

--- backend/api/views/recipes.py
+++ backend/api/views/recipes.py
@@ -10,7 +10,7 @@ from rest_framework import permissions, serializers, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from api.filters import IngredientFilter, RecipeFilter
+from api.filters import RecipeFilter
 from api.serializers.recipes import (FavoriteSerializer, IngredientSerializer,
                                      RecipeSerializer, RecipeSerializerWrite,
                                      ShoppingCartSerializer, TagSerializer)
